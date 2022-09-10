2ac680d054c7e11a6eb67e5f7a0d9f2cbc22b9d0
Lenkaa3792 <allanlenkaa@gmail.com>
2022-09-10 14:25:37 +0300

    seeding routes table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
 puts "🌱 Seeding spices..."
 
-Bus.create(insurance: "insured",phone: 78907654,vehicle_registration: "KCJ 123k")
-Bus.create(insurance: "uinsured",phone: 78906754,vehicle_registration: "KCL 183k")
+# Bus.create(insurance: "insured",phone: 78907654,vehicle_registration: "KCJ 123k")
+# Bus.create(insurance: "uinsured",phone: 78906754,vehicle_registration: "KCL 183k")
+# Bus.create(insurance: "insured",phone: 78907654,vehicle_registration: "KBJ 123G")
+# Bus.create(insurance: "insured",phone: 789654,vehicle_registration: "KCJ 173k")
 
+# Route.create(location: "Mombasa",name: "John Pombe")
+# Route.create(location: "Mombasa",name: "Stella Mwangi")
+# Route.create(location: "Nairobi",name: "mark Kibet")
+Passenger.create(name: 'John', phone: 8768768687, location: "kilifi")
+Passenger.create(name: 'John', phone: 8768768687, location: "kilifi")
+Passenger.create(name: 'John', phone: 8768768687, location: "kilifi")
 # Seed your database here
 
 puts "✅ Done seeding!"
