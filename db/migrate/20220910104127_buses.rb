937f459f1885c0a68e6767c7aaac840b2f21e6df
Lenkaa3792 <allanlenkaa@gmail.com>
2022-09-10 13:44:29 +0300

    buses migration

--- db/migrate/20220910104127_buses.rb
+++ db/migrate/20220910104127_buses.rb
@@ -1,4 +1,9 @@
-class Buses < ActiveRecord::Migration[6.1]
+class Buses < ActiveRecord::Migration[7.0]
   def change
+     create_table :buses do |t|
+     t.string :insurance
+     t.integer :phone
+     t.string :vehicle_registration
+       end
   end
 end
