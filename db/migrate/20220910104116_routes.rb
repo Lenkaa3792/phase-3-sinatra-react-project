4af47dd78879329aabd50da196bb6d087757053b
Lenkaa3792 <allanlenkaa@gmail.com>
2022-09-10 13:43:23 +0300

    routes migration

--- db/migrate/20220910104116_routes.rb
+++ db/migrate/20220910104116_routes.rb
@@ -1,4 +1,9 @@
-class Routes < ActiveRecord::Migration[6.1]
+class Routes < ActiveRecord::Migration[7.0]
   def change
+     create_table :routes do |t|
+      t.string :location
+      t.string :name
+
+    end
   end
 end
